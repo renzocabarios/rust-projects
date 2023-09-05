7d1c20f6543a1c10f156d7b563d87a5921851b33
renzocabarios <renzo.cabarios@gmail.com>
2023-09-05 22:00:56 +0800

    Add vector_iteration

---
+++ vector_iteration/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let v = vec!['G', 'E', 'E', 'K', 'S'];
+
+    print!("Vector elements : ");
+
+    //loop to iterate elements in vector
+    for i in v {
+        // iterating through i on the vector
+        print!("{} ", i);
+    }
+    print!("\n",);
+}
